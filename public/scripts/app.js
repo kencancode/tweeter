4447ee98c66d6d7357cfb638300b4cc657ea7284
Ken Thooptong <ken.thooptong@gmail.com>
2019-02-20 20:01:43 +0000

    all texts shown, including the new one after refreshing

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,53 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function(){
-// Fake data taken from tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
 
 function renderTweets(tweets) {
   // loops through tweets
@@ -76,10 +29,33 @@ function createTweetElement(tweet) {
           </article>`);
   return $tweet;
 }
-
 // $('#section-tweet').append(createTweetElement(data));
 
-renderTweets(data);
+// renderTweets(data);
+
+//prevent default form submission, serialize data submitted
+$(".tweet-form").on("submit", function (event){
+     event.preventDefault();
+     let serializedText =  $( this ).serialize();
+
+    $.post( "/tweets", serializedText)
+      .done(function(data, status){
+        console.log(status)
+      });
+  });
+
+
+function loadTweets (){
+  $.ajax('/tweets', {method: 'GET'})
+  .then(function(tweet){
+    renderTweets(tweet)
+  })
+}
+
+loadTweets()
+
+
+
 
 });
 
